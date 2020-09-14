06c9a58020619b2d710dd54b2eaba9bf542da9b8
Maikel Bakker <maikel.bakker@touchtribe.nl>
2020-09-14 15:17:43 +0200

    package.jsomn

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+import theme from '../theme'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Sustainable and honest jobs</title>
+        <meta name="robots" content="noindex, nofollow" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400&display=swap"
+          rel="preload"
+          as="style"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
